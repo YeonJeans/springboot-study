5c71558415db5df81fc7d938f8f6204f2dd76585
lizuAg <wndms058@gmail.com>
2022-12-04 23:35:24 +0900

    [chap3] 등록,수정,조회 API + JPA Auditing

---
+++ src/main/java/com/yeonjeans/springboot/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package com.yeonjeans.springboot.web;
+
+import com.yeonjeans.springboot.service.PostsService;
+import com.yeonjeans.springboot.web.dto.PostsResponseDto;
+import com.yeonjeans.springboot.web.dto.PostsSaveRequestDto;
+
+import com.yeonjeans.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
