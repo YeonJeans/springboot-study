1009379347427de8b741d89de0ba0c48b8586e3d
lizuAg <wndms058@gmail.com>
2022-12-11 23:50:55 +0900

    [chap5] google oauth

--- src/main/java/com/yeonjeans/springboot/web/IndexController.java
+++ src/main/java/com/yeonjeans/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.yeonjeans.springboot.web;
 
-import com.yeonjeans.springboot.domain.posts.PostsRepository;
+import com.yeonjeans.springboot.config.auth.LoginUser;
+import com.yeonjeans.springboot.config.auth.dto.SessionUser;
 import com.yeonjeans.springboot.service.PostsService;
 import com.yeonjeans.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
